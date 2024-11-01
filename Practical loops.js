8aabdb103a30382618b8b18b37ddb2957ce72d9f
AleahHunter16 <AleahHunter16@gmail.com>
2024-11-01 18:52:32 -0400

    changes

--- Practical loops.js
+++ Practical loops.js
@@ -11,10 +11,14 @@ for(let i = 1; i < 100; i++) {
 }
 
 //part two
-function isPrime(Number){
-    if(num<=1)return false;
-    if(num<=3)return true;
-    if( num%2===0;) num%3===0)return false;
+function isPrime(Num) {
+    if (num <= 1) return false;
+    if ( num <= 3) return true;
+    if ( num % 2 === 0 || num % 3 === 0) return false;
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num %ni === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
 
 }
 let n=4;
