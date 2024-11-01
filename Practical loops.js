57a8e935ea74d6b38e2990270dec9bd13e9c2ec4
AleahHunter16 <AleahHunter16@gmail.com>
2024-10-31 22:36:46 -0400

    commit

--- Practical loops.js
+++ Practical loops.js
@@ -3,4 +3,8 @@ const fizzbuzz=(n)=>{
     let out=''; if(n%3===0) out +="fizz";
     if(n%5===0) out =+ "buzz";
     return out===''?n:out;
+};
+
+for(let i=1;i<100;i++){
+    console.log(fizzbuzz(i));
 }
