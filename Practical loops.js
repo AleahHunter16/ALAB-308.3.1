5996fcdc2f4302179ef5d18ccc9e423ab8a12cc1
AleahHunter16 <AleahHunter16@gmail.com>
2024-11-01 18:33:39 -0400

    changes

--- Practical loops.js
+++ Practical loops.js
@@ -1,11 +1,12 @@
 // part one
-const fizzbuzz=(n)=>{
-    let out=''; if(n%3===0) out +="fizz";
-    if(n%5===0) out =+ "buzz";
-    return out===''?n:out;
+const fizzbuzz =(n) =>{
+    let out=''; 
+    if(n % 3 === 0) out += "fizz";
+    if(n % 5 ===0) out += "buzz";
+    return out ===''? n : out;
 };
 
-for(let i=1;i<100;i++){
+for(let i = 1; i < 100; i++) {
     console.log(fizzbuzz(i));
 }
 
